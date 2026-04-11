7fb7ea60bc686e2303627d04f5af5436bdd6dae1
david.schkalee <david.schkalee@sportalliance.com>
2026-04-12 00:55:51 +0200

    Bump up sdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ val keystoreProperties = Properties().also { props ->
 
 android {
     namespace = "com.raofflineproxy"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.raofflineproxy"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
     }
