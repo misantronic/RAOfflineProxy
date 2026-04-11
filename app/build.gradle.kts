fb6efb16c6a8643a535c1060345f4d1157dc7195
david.schkalee <david.schkalee@sportalliance.com>
2026-04-12 01:16:23 +0200

    Bump up targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     defaultConfig {
         applicationId = "com.raofflineproxy"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
     }
