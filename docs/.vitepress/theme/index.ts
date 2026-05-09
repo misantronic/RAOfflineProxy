edb2611e85de782f42a5809b8a241bcf300fd513
david.schkalee <david.schkalee@sportalliance.com>
2026-05-09 22:35:45 +0200

    Remove Ko-fi support widget from documentation theme

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
-import { h } from 'vue'
-import SupportKofiWidget from './components/SupportKofiWidget.vue'
 import './style.css'
 
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('SupportKofiWidget', SupportKofiWidget)
-  },
-  Layout() {
-    return h('div', [
-      h(DefaultTheme.Layout),
-      h(SupportKofiWidget),
-    ])
-  },
-}
+export default DefaultTheme
