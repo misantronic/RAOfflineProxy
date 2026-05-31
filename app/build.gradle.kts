77080cbc1147dcfbab604f957bb3990f3a9ed55a
david.schkalee <david.schkalee@sportalliance.com>
2026-05-31 22:04:00 +0200

    Release v1.3.0-alpha1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.raofflineproxy"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.2.2-alpha1"
+        versionCode = 13
+        versionName = "1.3.0-alpha1"
     }
 
     signingConfigs {
